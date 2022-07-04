6ba188168c3e19ee068a8e3d157ad6587be9c6ac
deomore <deomoremv@gmail.com>
2022-07-05 00:35:01 +0300

    Initial commit

---
+++ src/test/java/com/wcr/lable/LableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wcr.lable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
